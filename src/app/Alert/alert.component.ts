885b8529c2a78a579481147c9c6d47741b239072
michalWochna <mich.wochna@gmail.com>
2020-03-04 14:21:35 +0100

    1 file modified, 1 added, 1 deleted

--- src/app/Alert/alert.component.ts
+++ src/app/Alert/alert.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { AlertService } from './../services/alert-service.service';
+
 
 @Component({ selector: 'alert', templateUrl: 'alert.component.html' })
 export class AlertComponent implements OnInit, OnDestroy {
